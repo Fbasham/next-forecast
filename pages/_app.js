bc79374c554a5bb573d60a7ac75e432dc3b11f3d
Frank <48450599+Fbasham@users.noreply.github.com>
2022-08-17 08:42:37 -0400

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }) {
+  /* istanbul ignore next */
+  if (Component.getLayout) {
+    return Component.getLayout(<Component {...pageProps} />)
+  }
+
+  /* istanbul ignore next */
+  return (
+    <Layout
+      locale={pageProps.locale}
+      meta={pageProps.meta}
+      langToggleLink={pageProps.langToggleLink}
+    >
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
