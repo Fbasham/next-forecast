ea1394095566fcb44f75b7d5688fd911840fe243
Frank <frankabasham@gmail.com>
2022-08-20 18:19:15 -0400

    add accessibility to interactive map

--- pages/home/map.js
+++ pages/home/map.js
@@ -7,6 +7,8 @@ import About from '../../components/About'
 
 export default function Map() {
   let [coord, setCoord] = useState([42.9746, -82.4066])
+  let [lat, setLat] = useState(coord[0])
+  let [lon, setLon] = useState(coord[1])
   let [data, setData] = useState({})
 
   useEffect(() => {
@@ -23,15 +25,49 @@ export default function Map() {
     <div className="container p-8 px-6 mx-auto mt-5">
       <Breadcrumb />
       <h1 className="mb-4 text-4xl">Weather by Coordinates</h1>
-      <Mapp
-        height={300}
-        defaultCenter={coord}
-        defaultZoom={10}
-        onClick={(e) => setCoord(e.latLng)}
-      >
-        <Marker width={25} anchor={coord} />
-      </Mapp>
-      <div className="flex gap-10 mt-5">
+      <div>
+        <Mapp
+          height={300}
+          center={[lat, lon]}
+          defaultZoom={10}
+          onClick={(e) => setCoord(e.latLng)}
+        >
+          <Marker width={25} anchor={[lat, lon]} />
+        </Mapp>
+        <div className="flex flex-col md:flex-row md:justify-center md:items-center">
+          <lebel htmlFor="lat" className="font-semibold" className="mr-2">
+            Set Latitude
+          </lebel>
+          <input
+            type="number"
+            step=".01"
+            id="lat"
+            name="lat"
+            value={lat}
+            onChange={(e) => setLat(+e.target.value)}
+            className="border-2 rounded-md py-1 px-2 md:mr-2"
+          ></input>
+          <lebel htmlFor="lon" className="font-semibold" className="mr-2">
+            Set Longitude
+          </lebel>
+          <input
+            type="number"
+            step=".01"
+            id="lon"
+            name="lon"
+            value={lon}
+            onChange={(e) => setLon(+e.target.value)}
+            className="border-2 rounded-md py-1 px-2 md:mr-2"
+          ></input>
+          <button
+            onClick={(e) => setCoord([lat, lon])}
+            className="rounded-md bg-slate-600 hover:bg-slate-800 py-1 text-white px-2"
+          >
+            Fetch Weather
+          </button>
+        </div>
+      </div>
+      <div className="md:flex gap-10 mt-5">
         {!!Object.keys(data).length && <Table data={data} />}
         {!!Object.keys(data).length && <CityDetails data={data} />}
       </div>
