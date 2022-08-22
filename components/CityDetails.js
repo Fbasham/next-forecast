1ee76f06714dba11b27e7ea3888ac27aa0695738
Frank <frankabasham@gmail.com>
2022-08-22 14:22:56 -0400

    update CityDetails.js

--- components/CityDetails.js
+++ components/CityDetails.js
@@ -3,15 +3,15 @@ export default function CityDetails({ data }) {
     <div>
       <dl>
         <dt className="font-bold">Population</dt>
-        <dd>{data.city.population}</dd>
+        <dd>{data.city?.population}</dd>
         <dt className="font-bold">Latitude</dt>
-        <dd>{data.city.coord.lat}</dd>
+        <dd>{data.city?.coord.lat}</dd>
         <dt className="font-bold">Longitude</dt>
-        <dd>{data.city.coord.lon}</dd>
+        <dd>{data.city?.coord?.lon}</dd>
         <dt className="font-bold">Sunrise</dt>
-        <dd>{new Date(data.city.sunrise * 1000).toLocaleTimeString()}</dd>
+        <dd>{new Date(data.city?.sunrise * 1000).toLocaleTimeString()}</dd>
         <dt className="font-bold">Sunset</dt>
-        <dd>{new Date(data.city.sunset * 1000).toLocaleTimeString()}</dd>
+        <dd>{new Date(data.city?.sunset * 1000).toLocaleTimeString()}</dd>
       </dl>
     </div>
   )
