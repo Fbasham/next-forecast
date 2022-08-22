a6be3416bdd37132e5bd89709066e86962b0e578
Frank <48450599+Fbasham@users.noreply.github.com>
2022-08-22 12:58:43 -0400

    Update MetaData.js

--- components/MetaData.js
+++ components/MetaData.js
@@ -14,7 +14,7 @@ export default function MetaData(props) {
         <meta name="author" content={d?.author} />
         <meta name="keywords" content={d?.keywords} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="Content-Security-Policy" content="'nonce-{script value}' 'strict-dynamic' https: 'unsafe-eval'; img-src 'self' https://*.openstreetmap.org/; style-src https://*.openstreetmap.org/; connect-src 'self' https://*.openstreetmap.org/; frame-src https://*.openstreetmap.org/>
+        <meta httpEquiv="Content-Security-Policy" content="'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' https://*.openstreetmap.org/; style-src 'self' 'unsafe-inline' https://*.openstreetmap.org/; connect-src 'self' https://*.openstreetmap.org/; frame-src https://*.openstreetmap.org/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
