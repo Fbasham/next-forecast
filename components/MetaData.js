03831a62cdd88b0c4d438785c9b62efd0abdb71a
Frank <48450599+Fbasham@users.noreply.github.com>
2022-08-22 12:31:42 -0400

    Update MetaData.js

--- components/MetaData.js
+++ components/MetaData.js
@@ -14,6 +14,7 @@ export default function MetaData(props) {
         <meta name="author" content={d?.author} />
         <meta name="keywords" content={d?.keywords} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="Content-Security-Policy" content="script-src 'self' https://c.tile.openstreetmap.org; style-src 'self';"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
