4e3035d236f4fd850b188b81efc774dfe15f7723
Frank <48450599+Fbasham@users.noreply.github.com>
2022-08-22 12:44:15 -0400

    Update MetaData.js

--- components/MetaData.js
+++ components/MetaData.js
@@ -14,7 +14,7 @@ export default function MetaData(props) {
         <meta name="author" content={d?.author} />
         <meta name="keywords" content={d?.keywords} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta httpEquiv="Content-Security-Policy" content="script-src 'unsafe-inline'; style-src 'self'"/>
+        <meta httpEquiv="Content-Security-Policy" content="script-src 'self'; style-src 'self'"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
